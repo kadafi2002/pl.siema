c79a95c60f74481f4c900f6b73cb53b32bb0fe8a
kadafi2002 <kadafi2002@gmail.com>
2021-02-16 17:25:28 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+     // System.out.println("podaj swoje imię");
+      //Scanner scanner = new Scanner (System.in);
+     // String firstName = scanner.nextLine();
+     // System.out.println("Twoje imię to " + firstName );
+    //  scanner.close();
+  
+  
+            // System.out.print("Podaj wartosc A:");
+           //   int A;
+           //   Scanner odczyt1 = new Scanner(System.in);
+           //   A = odczyt1.nextInt();
+//System.out.print("Podaj wartosc B:");
+        //      int B;
+          //    Scanner odczyt2 = new Scanner(System.in);
+        //      B = odczyt2.nextInt();
+         //     System.out.print("Podaj wartosc C:");
+         //     int C;
+          //    Scanner odczyt3 = new Scanner(System.in);
+        //      C = odczyt3.nextInt();
+
+        //      System.out.println(A+"+"+B+"+"+C=);
+    }
+}
