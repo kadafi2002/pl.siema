bffb3968c67d51d04b537f78fcdd3e99791c1ea5
kadafi2002 <kadafi2002@gmail.com>
2021-02-16 18:45:45 +0100

    kalk

--- src/App.java
+++ src/App.java
@@ -8,20 +8,49 @@ public class App {
      // System.out.println("Twoje imię to " + firstName );
     //  scanner.close();
   
-  
-            // System.out.print("Podaj wartosc A:");
-           //   int A;
-           //   Scanner odczyt1 = new Scanner(System.in);
-           //   A = odczyt1.nextInt();
-//System.out.print("Podaj wartosc B:");
-        //      int B;
-          //    Scanner odczyt2 = new Scanner(System.in);
-        //      B = odczyt2.nextInt();
-         //     System.out.print("Podaj wartosc C:");
-         //     int C;
-          //    Scanner odczyt3 = new Scanner(System.in);
-        //      C = odczyt3.nextInt();
 
-        //      System.out.println(A+"+"+B+"+"+C=);
-    }
+        //      System.out.println(A+"+"+B+"+"+C=)
+         // KALKULATOR DWÓCH LICZB 
+       // System.out.println("Podaj pierwszą liczbę");
+        //int a = scanner.nextInt();
+        //System.out.println("Podaj drugą licfzbę");      
+        //int b = scanner.nextInt();
+        //System.out.println(a+" + "+b+" = "+(a+b));      
+         // IF ELSE
+       // int nr = 10;
+        //if(nr == 2){
+        //  System.out.println("Liczba to 2");
+       // }else if(nr == 3){
+       //   System.out.println("Liczba to 3");
+      //  }else{
+       //   System.out.println("Jakaś inna liczba niż 3 i 2");
+       Scanner scanner = new Scanner(System.in);
+     
+       System.out.println("Wpisz pierwszą liczbę");
+      int a = scanner.nextInt();
+
+      System.out.println("Podaj operator + lub -");
+      scanner.nextLine();
+      String operator = scanner.nextLine();
+      
+      System.out.println("Wpisz drugą liczbę");
+      int b = scanner.nextInt();
+
+      if (operator equal "-"){
+        System.out.println("Wynik to: " +(a-b));
+        }
+        else if(operator equal "+"){
+            System.out.println("Wynik to: " +(a+b));
+        }
+        else{
+            System.out.println("Wpisz odpowiedni operator. Spróbój ponownie.");
+     
+        }
+        
+      scanner.close();
+      
+      
+    
+   
+      }
 }
