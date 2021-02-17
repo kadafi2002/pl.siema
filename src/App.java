c091302cb43009371ef085bd911aaab8a560e48d
kadafi2002 <kadafi2002@gmail.com>
2021-02-17 16:18:51 +0100

    lepszy kalkulator

--- src/App.java
+++ src/App.java
@@ -24,31 +24,68 @@ public class App {
        //   System.out.println("Liczba to 3");
       //  }else{
        //   System.out.println("Jakaś inna liczba niż 3 i 2");
-       Scanner scanner = new Scanner(System.in);
+     //  Scanner scanner = new Scanner(System.in);
      
-       System.out.println("Wpisz pierwszą liczbę");
-      int a = scanner.nextInt();
+      // System.out.println("Wpisz pierwszą liczbę");
+     // int a = scanner.nextInt();
 
-      System.out.println("Podaj operator + lub -");
-      scanner.nextLine();
-      String operator = scanner.nextLine();
+    //  System.out.println("Podaj operator + lub -");
+     // scanner.nextLine();
+     // String operator = scanner.nextLine();
       
-      System.out.println("Wpisz drugą liczbę");
-      int b = scanner.nextInt();
+    //  System.out.println("Wpisz drugą liczbę");
+    //  int b = scanner.nextInt();
 
-      if (operator equal "-"){
-        System.out.println("Wynik to: " +(a-b));
-        }
-        else if(operator equal "+"){
-            System.out.println("Wynik to: " +(a+b));
-        }
-        else{
-            System.out.println("Wpisz odpowiedni operator. Spróbój ponownie.");
+    //  if (operator.equals ("-")){
+    //    System.out.println("Wynik to: " +(a-b));
+     //   }
+     //   else if(operator.equals ("+")){
+    //        System.out.println("Wynik to: " +(a+b));
+     //   }
+     //   else{
+     //       System.out.println("Wpisz odpowiedni operator. Spróbój ponownie.");
      
-        }
+//}
         
-      scanner.close();
+      //scanner.close();
+    
+    
+      Scanner scanner = new Scanner(System.in); 
+      System.out.println("Podaj pierwszą liczbę:");
+      int a= scanner.nextInt();
+      System.out.println("Podaj + lub - lub % lub * lub /");
+      scanner.nextLine();
+      String o= scanner.nextLine(); 
+      System.out.println("Podaj drugą liczbe liczbę:");
+       int c= scanner.nextInt();
+  
+
+       switch (o) {
+           case "+": 
+           System.out.println("wynik dodawania " + (a+c));
+           break;
       
+        
+          case "-":
+          System.out.println("wynik odejmowania " + (a-c));
+          break;
+      
+          
+          case "*": 
+          System.out.println("wynik mnożenia " + (a*c));
+          break;
+      
+         
+          case "/": 
+          System.out.println("wynik dzielenia " + (a/c));
+          break;
+      
+      
+          case "%":
+          System.out.println("wynik reszty z dzielenia " + (a%c));
+          break;
+      }
+scanner.close();
       
     
    
